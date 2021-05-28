38dd228413e9868a994e522c14c08f74f78b359b
Ankita-Dake <ankitadake1997@gmail.com>
2021-05-28 12:04:28 +0530

    first commit

---
+++ module.py
@@ -0,0 +1,25 @@
+# create file fist module.py
+def showmessage(msg):
+    print("welcome to", msg)
+
+
+def func(name, address):
+    print("my name is", name)
+    print("address is", address)
+
+
+a = int(input("enter number1"))
+b = int(input("enter number2 "))
+
+
+class demo:
+    def func1(self):
+        print("hello")
+
+    def func2(self):
+        print("python")
+
+
+class demo2(demo):
+    def abc(self):
+        print("message")
---
+++ next.py
@@ -0,0 +1,11 @@
+import module
+module.showmessage("python")
+module.func('om', 'pune')
+i = module.a
+j = module.b
+print('sum is', i+j)
+
+d = module.demo2()
+d.func1()
+d.func2()
+d.abc()
